b5bc82c21228584f737d8e0d8dd7fb275057e9c1
Anders Strömberg <coltlaboy@yahoo.se>
2023-05-14 20:46:04 +0200

    First commit

---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+import heroImg from './assets/hero.svg'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+            Carrot cake caramels pie sweet apple pie tiramisu carrot cake.
+            Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon
+            drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton
+            candy gummi bears chocolate bar cake cookie. Cupcake muffin danish
+            muffin cookie gummies.
+          </p>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/fetchProjects.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { createClient } from 'contentful'
+
+const SPACE_ID = import.meta.env.VITE_SPACE_ID
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN
+
+const client = createClient({
+  space: SPACE_ID,
+  environment: 'master',
+  accessToken: ACCESS_TOKEN,
+})
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' })
+
+      const projects = response.items.map(item => {
+        const { title, url, image } = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+
+        return { title, url, id, img }
+      })
+
+      setProjects(projects)
+    } catch (error) {
+      console.log(error)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return { loading, projects }
+}
